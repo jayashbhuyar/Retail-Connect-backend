e5ee824ad2165e718a0877e6b9c42eaf8c910413
jayashbhuyar <150058929+jayashbhuyar@users.noreply.github.com>
2024-10-30 21:47:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ const PORT = process.env.PORT || 8000;
 const corsOptions = {
   origin: "https://retail-connect-webapp.vercel.app", // Update this to your frontend URL
   credentials: true, // Allow cookies to be sent with requests
+  methods: ['GET', 'POST','PUT','DELETE'], // Add other methods if needed
   optionsSuccessStatus: 200,
 };
 
